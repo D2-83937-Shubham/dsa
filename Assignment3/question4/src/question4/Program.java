a4c2e0d1635f47ba51faafd98f86cb7d67d326bc
shubhamg83937 <shubhamgade562@gmail.com>
2024-06-24 09:32:32 +0530

    Assignment 3 Question 4 is completed

---
+++ Assignment3/question4/src/question4/Program.java
@@ -0,0 +1,17 @@
+package question4;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		SLL ll = new SLL();
+		ll.insertInSortedOrder(40);
+		ll.insertInSortedOrder(36);
+		ll.insertInSortedOrder(20);
+		ll.insertInSortedOrder(65);
+		
+		ll.display();
+	}
+
+}
