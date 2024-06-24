13149c25fbfdaf61a14e1a727449c07b5506eb66
shubhamg83937 <shubhamgade562@gmail.com>
2024-06-24 09:35:46 +0530

    Assignment 5 Question 1 is completed

---
+++ Assignment5/question1/src/question1/Program.java
@@ -0,0 +1,25 @@
+package question1;
+
+//1. Write recursive function to add node into bst.
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		BST bst = new BST();
+		bst.addNodeRec(8);
+		bst.addNodeRec(3);
+		bst.addNodeRec(10);
+		bst.addNodeRec(6);
+		bst.addNodeRec(1);
+		bst.addNodeRec(14);
+		bst.addNodeRec(13);
+		bst.addNodeRec(7);
+		bst.addNodeRec(4);
+			
+		bst.preorder();
+
+		
+	}
+
+}
