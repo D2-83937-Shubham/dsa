224a001c2694ae32eaa1bfd85fc3a9d752aa09fc
shubhamg83937 <shubhamgade562@gmail.com>
2024-06-24 09:35:55 +0530

    Assignment 5 Question 3 is completed

---
+++ Assignment5/question3/src/question3/Program.java
@@ -0,0 +1,25 @@
+package question3;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		BST bst = new BST();
+		bst.addNode(8);
+		bst.addNode(3);
+		bst.addNode(10);
+		bst.addNode(6);
+		bst.addNode(1);
+		bst.addNode(14);
+		bst.addNode(13);
+		bst.addNode(7);
+		bst.addNode(4);
+			
+		bst.preorder();
+		bst.deleteNode(8);
+		bst.preorder();
+
+		
+	}
+
+}
