fccd40cc9a24b7a07946697c0b59a4621b548a1f
shubhamg83937 <shubhamgade562@gmail.com>
2024-06-24 09:32:01 +0530

    Assignment 3 Question 2 is completed

---
+++ Assignment3/question2/src/question2/Program.java
@@ -0,0 +1,21 @@
+package question2;
+
+//2. In singly linear list implement following operations. i. insert a new node after a given node ii. insert a
+//new node before a given node
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		SLL ll = new SLL();
+		ll.addFirst(40);
+		ll.addFirst(30);
+		ll.addFirst(20);
+		ll.addFirst(10);
+		ll.addAfter(100, 30);
+		ll.addBefore(200, 20);
+		ll.display();
+
+	}
+
+}
